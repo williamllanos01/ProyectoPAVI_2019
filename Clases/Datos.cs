ca6d875cb3c6a99897f8d18ecdb4ad2e7a67467d
sergiogc008 <54327704+sergiogc008@users.noreply.github.com>
2019-08-20 17:09:20 -0300

    Add files via upload

---
+++ Clases/Datos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1.Clases
+{
+    class Datos
+    {
+        //conectar a la bd
+        private OleDbConnection conexion = new OleDbConnection();
+        //vamos a usar comandos 
+        private OleDbCommand comando = new OleDbCommand();
+        private string cadenaConexion = @"Provider=SQLNCLI11;Data Source=maquis;User ID=avisuales1;Initial Catalog=BD_bugs;Password=avisuales1";
+        //DEFINIR METODOS PARA ESTA CLASE
+        private void conectar()
+        {
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+            comando.Connection = conexion;
+            comando.CommandType=CommandType.Text;
+        }
+        private void desconectar()
+        {
+            conexion.Close();
+        }
+        public DataTable consultar(string consultaSQL)
+        {
+            DataTable tabla = new DataTable();
+            this.conectar();
+            comando.CommandText=consultaSQL;
+            comando.CommandText = consultaSQL;
+            tabla.Load(comando.ExecuteReader());
+            return tabla;
+        }
+    }
+}
---
+++ Clases/Usuario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace WindowsFormsApplication1.Clases
+{
+    public class Usuario
+    {
+        int id_usuario;
+        string n_usuario;
+        string password;
+        string email;
+        int id_perfil;
+
+        
+
+        public int Id_usuario
+        {
+            get { return id_usuario; }
+            set { id_usuario = value; }
+        }
+        public string N_usuario
+        {
+            get { return n_usuario; }
+            set { n_usuario = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public int Id_perfil
+        {
+            get { return id_perfil; }
+            set { id_perfil = value; }
+        }
+
+        public int validadUsuario(string nombre, string clave)
+        {
+            DataTable Tabla = new DataTable();
+            string consultarSQL = "SELECT * FROM Users WHERE n_usuario='"+nombre+"' AND password='"+clave+"'";
+            Datos oBD = new Datos();
+            Tabla = oBD.consultar(consultarSQL);
+            if (Tabla.Rows.Count>0)
+                return Convert.ToInt32(Tabla.Rows[0][0]);
+            else
+                return 0;
+        }
+
+    }
+}
